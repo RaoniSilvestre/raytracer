e118f48ece3bab5ec5b72ad57c749b83e5f699b8
arthurjoseas <arthurjoskeete@gmail.com>
2026-04-11 17:15:46 -0300

    forgot to add these files

--- src/object/sphere.cpp
+++ src/object/sphere.cpp
@@ -2,30 +2,30 @@
 #include "object/sphere.hpp"
 #include "math/vector_3.hpp"
 #include <cmath>
-#include <ios>
-#include <iostream>
 
 bool Sphere::intersect_p(const Ray &r) const {
-  std::ios_base::sync_with_stdio(false);
-  auto o_c = r.origem - center;
-  auto d = r.direcao;
-  float a = dot(d, d); // d . d
-  float h = dot(o_c, d);//o-c dot d
-  float c = dot(o_c, o_c); //o-c dot o-c
-  float delta = (h * h - (a * c-radius*radius));
-
+  Vector3 o_c = r.origem - center;
+  Vector3 d = r.direcao;
+  Vector3 d_hat = unit_vector(d);
+  double parallel_length = dot(o_c, d_hat);
+  Vector3 o_c_perp = o_c - parallel_length*d_hat;
+  double delta = radius*radius-(dot(o_c_perp, o_c_perp));
+  
   // Delta negativo significa que não intersecta.
   if (delta < 0) {
     return false;
   }
   return true;
-  float sqrt_delta = sqrt(delta);
 
-  float t = (-h - sqrt_delta) / a;
+  auto dd = dot(d, d);
+  auto o_cd = dot(o_c, d);
+  double sqrt_delta = sqrt(delta);
 
-  if (t <= 0) {
-    t = (-h + sqrt_delta) / a;
-    if (t <= 0) {
+  double t = (-o_cd - sqrt_delta) / dd;
+
+  if (t < 0) {
+    t = (-o_cd + sqrt_delta) / dd;
+    if (t < 0) {
       return false;
     }
   }
